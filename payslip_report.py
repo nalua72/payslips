c3c7ec82a4063b5e42c96029a1ea4da9b268d015
Jose Rodriguez <nalua72@gmail.com>
2024-11-15 17:25:20 +0100

    * modified regex expression * Created function dataframe_clean() and moved dafarme formatting there

--- payslip_report.py
+++ payslip_report.py
@@ -1,8 +1,7 @@
-import os
-import re
 import pandas as pd
 from pathlib import Path
 from tabula import read_pdf
+import re
 
 # Function to extract a table from a file a return a dataframe. A DATA column is added.
 
@@ -14,17 +13,46 @@ def pdftable_to_dataframe(file, area, columns, page):
     df.insert(0, "FECHA", [file.name.split("_")[0] for _ in range(len(df))])
     return df
 
+# Function that takes a df with payslips and formats it
+
+
+def dataframe_clean(df):
+    # Name the columns
+    df.columns = ["FECHA", "CUANTIA", "PRECIO",
+                  "CODIGO", "CONCEPTO", "DEVENGOS", "DEDUCCIONES"]
+    df = df.reset_index(drop=True)
+    df = df.fillna(0)
+
+    # Change thousands separator from , to .
+    df['CUANTIA'] = df['CUANTIA'].str.replace(',', '.')
+    df['PRECIO'] = df['PRECIO'].str.replace(',', '.')
+    df['DEVENGOS'] = df['DEVENGOS'].str.replace('.', '')
+    df['DEVENGOS'] = df['DEVENGOS'].str.replace(',', '.')
+    df['DEDUCCIONES'] = df['DEDUCCIONES'].str.replace(',', '.')
+
+    # Modifies the columns to get the right format
+    column_modify = ["CUANTIA", "PRECIO", "DEVENGOS", "DEDUCCIONES"]
+    df[column_modify] = df[column_modify].astype(float)
+    df['FECHA'] = pd.to_datetime(df['FECHA'], format='%Y%m%d').dt.date
+    df['CODIGO'] = df['CODIGO'].astype(int)
+    df = df.fillna(0)
+
+    # Adds column Balance
+    df['BALANCE'] = df['DEVENGOS'] - df['DEDUCCIONES']
+    # Drop empty rows
+
+    filt = df['CONCEPTO'] == 0
+    df = df.drop(index=df[filt].index)
+    return df
+
 
 def main():
     data = []
-    # thisdir = r"Z:\Jose\Nominas\Cognizant"
-    # thisdir = os.getcwd()
 
     base_path = Path(r"Z:\Jose\Nominas\Cognizant")
     files = list(base_path.glob("*.pdf"))
 
     for file in files:
-
         if file.name.split("_")[0] != "20220430":
             area = [38, 0, 63, 100]
             columns = [15, 25, 31, 69, 80]
@@ -32,8 +60,7 @@ def main():
             data.append(pdftable_to_dataframe(file, area, columns, 1))
 
             # For march months (except for 2016 and 2017) there are 2 pages in the excel because of the BONUS. Table is extracted from the second page.
-            if re.search("[0-9][0-9][0-9][0-9]03[0-9][0-9]", file.name) and file.name.split("_")[0] != "20160331" and file.name.split("_")[0] != "20170331":
-
+            if re.search("\d{4}03\d{2}", file.name) and file.name.split("_")[0] != "20160331" and file.name.split("_")[0] != "20170331":
                 data.append(pdftable_to_dataframe(file, area, columns, 2))
         else:
             area = [36, 0, 63, 100]
@@ -42,54 +69,14 @@ def main():
             data.append(data.append(
                 pdftable_to_dataframe(file, area, columns, 1)))
 
-    # for file in os.listdir(thisdir):
-    #     # For every payslip file, a dataframe with the table is stored in a list
-    #     if "pdf" in file:
-    #         if file.split("_")[0] != "20220430":
-    #             area = [38, 0, 63, 100]
-    #             columns = [15, 25, 31, 69, 80]
-
-    #             data.append(pdftable_to_dataframe(file, area, columns, 1))
-
-    #             # For march months (except for 2016 and 2017) there are 2 pages in the excel because of the BONUS. Table is extracted from the second page.
-    #             if re.search("[0-9][0-9][0-9][0-9]03[0-9][0-9]", file) and file.split("_")[0] != "20160331" and file.split("_")[0] != "20170331":
-
-    #                 data.append(pdftable_to_dataframe(file, area, columns, 2))
-    #         else:
-    #             area = [36, 0, 63, 100]
-    #             columns = [15, 25, 30, 67, 78]
-
-    #             data.append(data.append(
-    #                 pdftable_to_dataframe(file, area, columns, 1)))
-
     # A DataFrame is created with all the dataframes stored in the list
     df = pd.concat(data)
 
-    df.columns = ["FECHA", "CUANTIA", "PRECIO", "CODIGO",
-                  "CONCEPTO", "DEVENGOS", "DEDUCCIONES"]
-    df = df.reset_index(drop=True)
-    df = df.fillna(0)
-
-    df['CUANTIA'] = df['CUANTIA'].str.replace(',', '.')
-    df['PRECIO'] = df['PRECIO'].str.replace(',', '.')
-    df['DEVENGOS'] = df['DEVENGOS'].str.replace('.', '')
-    df['DEVENGOS'] = df['DEVENGOS'].str.replace(',', '.')
-    df['DEDUCCIONES'] = df['DEDUCCIONES'].str.replace(',', '.')
-
-    column_modify = ["CUANTIA", "PRECIO", "DEVENGOS", "DEDUCCIONES"]
-    df[column_modify] = df[column_modify].astype(float)
-    df['FECHA'] = pd.to_datetime(df['FECHA'], format='%Y%m%d').dt.date
-    df['CODIGO'] = df['CODIGO'].astype(int)
-    df = df.fillna(0)
-
-    df['NOMINA'] = df['DEVENGOS'] - df['DEDUCCIONES']
-    # Drop empty rows
-
-    filt = df['CONCEPTO'] == 0
-    df = df.drop(index=df[filt].index)
+    # Dataframe cleaning
+    df_clean = dataframe_clean(df)
 
     # DataFrame is stored in excel file
-    df.to_excel('payslip.xlsx', index=False)
+    df_clean.to_excel('payslip.xlsx', index=False)
 
 
 if __name__ == "__main__":
